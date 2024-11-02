7ac5b3d540f986f4766dfd1d6db499df42b21d0d
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-11-02 16:20:31 +0200

    feat: add HomePage
    - Implement Hero and add it to hompage
    - Implement Footer and add it to homepage
    - add hero-section image to public dir
    
    fixs #22

---
+++ src/components/home/HomePage.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+import Footer from "../Shared/Footer";
+import Hero from "./Hero";
+
+/** TODO:
+ * ✅ non-user navbar
+ * add homepage body
+ *
+ */
+const HomePage = () => {
+  return (
+    <Box display="flex" flexDirection="column" flex="1">
+      <Hero />
+      <Footer />
+    </Box>
+  );
+};
+
+
+export default HomePage;
